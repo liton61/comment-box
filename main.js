a0231f94c24012d6bcff2664c5dc09ec41ec2b77
litonofficial61 <litonofficial61gmail.com>
2023-08-13 13:41:42 +0600

    comment box

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById('post-btn').addEventListener('click', function () {
+    const commentBox = document.getElementById('comment-box');
+    const newComment = commentBox.value;
+
+
+    const commentContainer = document.getElementById('comment-container');
+    const p = document.createElement('p');
+    p.innerText = newComment;
+    commentContainer.appendChild(p);
+
+    commentBox.value = '';
+})
